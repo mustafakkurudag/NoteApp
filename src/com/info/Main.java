c141c647444038541d4c52b72df9e86fc816f377
mustafakkurudag <mustafakurudag@gmail.com>
2024-05-13 16:59:36 +0300

    functions added

--- src/com/info/Main.java
+++ src/com/info/Main.java
@@ -5,76 +5,114 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
+    static List<String> optionList = new ArrayList<>();
     public static void main(String[] args) {
         int not;
         int selection = 9;
-        int deleteOption;
-        int deleteNoteSelection;
-        List<String> optionList = new ArrayList<>();
         List<Integer> notList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
-        //optionList'i dolduruyoruz
+        optionList.add("0 - Çıkış Yap");
         optionList.add("1 - Not Ekle");
         optionList.add("2 - Notları Görüntüle");
         optionList.add("3 - Not Sil");
-        optionList.add("0 - Çıkış Yap");
-
-        /**
-         *   System.out.println();//parantez içindeki değer ekrana yazıldıktan sonra imleç bir alt satıra iner
-         *   System.out.print("");//parantez içindeki değer ekrana yazıldıktan sonra imleç aynı satırda kalmaya devam  eder.
-         */
+        optionList.add("4 - Not Ortalamasını Hesapla");
 
         System.out.println("Hoşgeldiniz...");
 
         while (selection != 0) {
             //for foreach
-            for (int i = 0; i < optionList.size(); i++) {
-                System.out.println(optionList.get(i));
-            }
+            showMenu(optionList);
 
             System.out.println("Lütfen seçim yapınız: ");
             selection = scanner.nextInt();
 
             switch (selection) {
-                case 1:
-                    System.out.print("Lütfen not giriniz: ");
-                    not = scanner.nextInt();
-                    notList.add(not);
-                    System.out.println("Not başarıyla kaydedildi!");
-                    break;
-                case 2:
-                    System.out.print("Notlar: ");
-                    for (int n : notList) {
-                        System.out.print(n + ", ");
-                    }
-                    System.out.println();
-                    break;
                 case 0:
                     System.out.println("Uygulamadan çıkılıyor...");
                     break;
+                case 1:
+                    notList = saveExamScores(notList);
+                    break;
+                case 2:
+                    showAllExamScores(notList);
+                    break;
                 case 3:
-                    System.out.println("1 - Tüm notları sil");
-                    System.out.println("2 - Notu seçip sil");
-                    deleteOption = scanner.nextInt();
-                    if (deleteOption == 1) {
-                        notList.clear();
-                        System.out.println("Tüm notlar silindi!");
-                        break;
-                    } else if (deleteOption == 2) {
-                        System.out.print("Silmek istediğiniz notu seçiniz: ");
-                        System.out.println(notList);
-                        deleteNoteSelection = scanner.nextInt();
-                        int noteIndex = notList.indexOf(deleteNoteSelection);
-                        if (noteIndex != -1) {
-                            notList.remove(noteIndex);
-                            System.out.println("Girilen not başarıyla silindi!");
-                            break;
-                        }
-                    }
+                    notList = deleteNotes(notList);
+                    break;
+                case 4:
+                    double average = calculateAverage(notList);
+                    System.out.print("Girilen notların ortalaması: " + average);
+                    System.out.println("");
+                    break;
+            }
+        }
+    }
+
+    /**
+     * public static int findSumUsingStream(int[] array) {
+     *     return Arrays.stream(array).sum();
+     * }
+     */
+    private static List<Integer> saveExamScores(List<Integer> examScoreList) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Lütfen not giriniz: ");
+        int examScore = scan.nextInt();
+        examScoreList.add(examScore);
+        System.out.println("Not başarıyla kaydedildi!");
+
+        return examScoreList;
+    }
+    private static void showAllExamScores(List<Integer> examScoreList) {
+        System.out.print("Notlar: ");
+        System.out.println(examScoreList);
+    }
+    private static List<Integer> deleteNotes(List<Integer> examScoreList) {
+        System.out.println("1 - Tüm notları sil");
+        System.out.println("2 - Notu seçip sil");
+        /** new */System.out.println("3 - Ana menü");
+        Scanner scan = new Scanner(System.in);
+        int deleteOption = scan.nextInt();
+        if (deleteOption == 1) {
+            examScoreList.clear();
+            System.out.println("Tüm notlar silindi!");
+        } else if (deleteOption == 2) {
+            System.out.print("Silmek istediğiniz notu seçiniz: ");
+            System.out.println(examScoreList);
+            int deleteNoteSelection = scan.nextInt();
+            int noteIndex = examScoreList.indexOf(deleteNoteSelection);
+            if (noteIndex != -1) {
+                examScoreList.remove(noteIndex);
+                System.out.println("Girilen not başarıyla silindi!");
+            } else if(deleteOption == 3) {
+                showMenu(optionList);
+            } else {
+                System.out.println("Lütfen geçerli bir seçim yapınız!");
+                deleteNotes(examScoreList);
             }
         }
 
+        return examScoreList;
     }
+    private static double calculateAverage(List<Integer> examScores) {
+        double average = 0.0;
+        int sum = 0;
+
+        for (int not: examScores){
+            sum += not;
+        }
+
+        average = sum / examScores.size();
+
+        return average;
+    }
+
+    private static void showMenu(List<String> options) {
+        for (String option : options) {
+            System.out.println(option);
+        }
+    }
+
+
 }
